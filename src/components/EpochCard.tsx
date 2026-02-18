6eacb5be2a5432dcfee349590faf0cde4e79eefc
pswinka85-source <pswinka85@gmail.com>
2026-02-18 12:58:24 +0100

    Update EpochCard.tsx

--- src/components/EpochCard.tsx
+++ src/components/EpochCard.tsx
@@ -1,6 +1,5 @@
 import { Link } from "react-router-dom";
 import type { EpochData } from "@/data/epochs";
-import { ArrowRight } from "lucide-react";
 
 interface EpochCardProps {
   epoch: EpochData;
