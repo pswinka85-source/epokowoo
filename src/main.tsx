398d83729781aa4d5834dd0a5f687a58ea2a9df2
pswinka85-source <pswinka85@gmail.com>
2026-02-20 15:05:31 +0100

    Include Footer component in main.tsx
    Added Footer component to the main render method.

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,15 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { Footer } from "./components/Footer"; // import stopki
 
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+    <Footer /> {/* stopka na dole */}
+  </React.StrictMode>
+);
 createRoot(document.getElementById("root")!).render(<App />);
