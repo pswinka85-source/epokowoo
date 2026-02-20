99ed80ece36bdf46bd094236a328e784a3854f82
pswinka85-source <pswinka85@gmail.com>
2026-02-20 16:03:01 +0100

    Update footer links and copyright text

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,15 +1,12 @@
 import React from "react";
-import "./Footer.css"; // jeśli chcesz mieć osobny plik CSS
+import "./Footer.css";
 
 export const Footer = () => {
   return (
     <footer className="site-footer">
       <div className="footer-content">
-        <p>© 2026 Kurs Maturalny - Epokowo. Wszelkie prawa zastrzeżone.</p>
-        <p>
-          <a href="#">Polityka prywatności</a> | 
-          <a href="#">Kontakt</a>
-        </p>
+        <a href="#">Polityka Prywatności</a>
+        <a href="#">Ochrona danych</a>
       </div>
     </footer>
   );
