5efdd20197ce07b2adbc2030e549af69d0caee77
pswinka85-source <pswinka85@gmail.com>
2026-02-21 14:23:09 +0100

    sprawdzam

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "@/hooks/useAuth";
 import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
 import ScrollToTop from "./components/ScrollToTop";
 import { Footer } from "./components/Footer"; // <-- dodany import stopki
 
@@ -30,19 +31,22 @@ const App = () => (
           <ScrollToTop />
           <div className="min-h-screen flex flex-col">
             <Header />
-            <main className="flex-1">
-              <Routes>
-                <Route path="/" element={<Auth />} />
-                <Route path="/epoki" element={<Index />} />
-                <Route path="/epoka/:id" element={<EpochDetail />} />
-                <Route path="/profil" element={<Profile />} />
-                <Route path="/reset-password" element={<ResetPassword />} />
-                <Route path="/kontakt" element={<Contact />} />
-                <Route path="/egzaminy" element={<Exams />} />
-                <Route path="/admin" element={<AdminDashboard />} />
-                <Route path="*" element={<NotFound />} />
-              </Routes>
-            </main>
+            <div className="flex flex-1">
+              <Sidebar />
+              <main className="flex-1">
+                <Routes>
+                  <Route path="/" element={<Auth />} />
+                  <Route path="/epoki" element={<Index />} />
+                  <Route path="/epoka/:id" element={<EpochDetail />} />
+                  <Route path="/profil" element={<Profile />} />
+                  <Route path="/reset-password" element={<ResetPassword />} />
+                  <Route path="/kontakt" element={<Contact />} />
+                  <Route path="/egzaminy" element={<Exams />} />
+                  <Route path="/admin" element={<AdminDashboard />} />
+                  <Route path="*" element={<NotFound />} />
+                </Routes>
+              </main>
+            </div>
             <Footer />
           </div>
 
