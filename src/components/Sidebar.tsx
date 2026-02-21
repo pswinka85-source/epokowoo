e1a10dcff1c9b25479bdee75aa57cb350221b724
pswinka85-source <pswinka85@gmail.com>
2026-02-21 17:15:40 +0100

    Obniżenie białego kokpitu o 16px przez dodanie mt-4, napis 'Kokpit' bez zmian

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -19,7 +19,7 @@ const Sidebar = () => {
       <div className="mb-4 pt-12 md:pt-20">
         <h1 className="text-3xl md:text-4xl text-center leading-[1.1]">Kokpit</h1>
       </div>
-      <div className="w-[280px] bg-white p-4 flex flex-col rounded-2xl shadow-lg">
+      <div className="w-[280px] bg-white p-4 flex flex-col rounded-2xl shadow-lg mt-4">
         <nav className="flex-grow">
           <ul className="space-y-3">
             {navItems.map((item) => (
