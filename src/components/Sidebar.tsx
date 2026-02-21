42c7e871f0ceb9dedb873a687c2f7c3bd8b00a83
pswinka85-source <pswinka85@gmail.com>
2026-02-21 17:14:13 +0100

    Ustawienie 'Kokpit' na takie same pt jak 'Cześć' - pt-12 md:pt-20

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -16,7 +16,7 @@ const Sidebar = () => {
 
   return (
     <div className="mx-4 mb-4">
-      <div className="mb-4 mt-0 md:mt-6">
+      <div className="mb-4 pt-12 md:pt-20">
         <h1 className="text-3xl md:text-4xl text-center leading-[1.1]">Kokpit</h1>
       </div>
       <div className="w-[280px] bg-white p-4 flex flex-col rounded-2xl shadow-lg">
