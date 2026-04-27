cb2a7ce7f45f328eea8d94502255acd8657c8b26
pswinka85-source <pswinka85@gmail.com>
2026-04-27 20:29:48 +0200

    Update Profile.tsx to make settings fields full width

---
+++ Profile.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className="settings-container" style={{ width: '100%' }}> {/* Removed max-width */}
+            {/* Your settings fields here */}
+        </div>
+    );
+};
+
+export default Profile;
